4ab2fa6ed0a23796f45ae82c1cd74d1522bb1718
Ricardo Guerrero <ricardogro@outlook.com>
2026-04-30 14:00:59 -0700

    Add logic for basic operations

---
+++ script.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let number1;
+let number2;
+let operator;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+        default:
+            console.log("Invalid operator");
+            break;
+    }
+}
